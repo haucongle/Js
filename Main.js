344db5f823f644aad34e3806f00f94fdb410690d
Hau Le <leconghau.me@gmail.com>
2018-06-15 16:44:10 +0700

    minor

--- Main.js
+++ Main.js
@@ -2,13 +2,13 @@ function Polygon(sides) {
     this.sides = sides;
 }
 
-Polygon.prototype.perimeter = function() {
+Polygon.prototype.perimeter = function () {
     let prmt = 0;
     for (let side of this.sides) {
         prmt += side;
     }
     return prmt;
-}
+};
 
 const rectangle = new Polygon([10, 20, 10, 20]);
 const square = new Polygon([10, 10, 10, 10]);
