7195b8cd5c1cf3e25fb0c2fca249cf4d843b5d43
Hau Le <leconghau.me@gmail.com>
2018-06-20 23:28:52 +0700

    js10-regexp-1

--- Main.js
+++ Main.js
@@ -1,14 +1,6 @@
-function getDayName(date) {
-    let weekday = new Array(7);
-    weekday[0] = "Sunday";
-    weekday[1] = "Monday";
-    weekday[2] = "Tuesday";
-    weekday[3] = "Wednesday";
-    weekday[4] = "Thursday";
-    weekday[5] = "Friday";
-    weekday[6] = "Saturday";
-    let d = new Date(date);
-    return weekday[d.getDay()];
+function regexVar() {
+    return /^([aeiou]).*\1$/;
 }
 
-console.log(getDayName("11/10/2010"));
+const re = regexVar();
+console.log(re.test('bcd'));
