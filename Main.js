2941ddd681e37fdb5b6074b6a471e0654229b7c0
Hau Le <leconghau.me@gmail.com>
2018-06-20 17:04:12 +0700

    js10-bitwise

--- Main.js
+++ Main.js
@@ -1,15 +1,14 @@
-function sides(literals, ...expressions) {
-    let a = expressions[0];
-    let p = expressions[1];
-    let s1 = (p + Math.sqrt(p * p - 16 * a)) / 4;
-    let s2 = (p - Math.sqrt(p * p - 16 * a)) / 4;
-    return [s1, s2].sort();
+function getMaxLessThanK(n, k) {
+    let max = -1;
+    for (let i = 1; i <= n; i++) {
+        for (let j = i + 1; j <= n; j++) {
+           let tmp = i & j;
+           if (tmp > max && tmp < k) {
+               max = tmp;
+           }
+        }
+    }
+    return max;
 }
 
-const s1 = 10;
-const s2 = 14;
-
-const [x, y] = sides`The area is: ${s1 * s2}.\nThe perimeter is: ${2 * (s1 + s2)}.`;
-
-console.log((s1 === x) ? s1 : -1);
-console.log((s2 === y) ? s2 : -1);
+console.log(getMaxLessThanK(2, 2));
